4351a916f01a28db04b1d82a9b5da483ba0f4406
James-Price <james.price@ucl.ac.uk>
2024-08-15 15:58:58 +0100

    Add new run_gams script that "should" be a platform independent way to execute gams within snakemake

---
+++ workflow/scripts/run_gams.py
@@ -0,0 +1,28 @@
+import subprocess
+
+# TODO - test on linux
+
+args=[snakemake.params.gamspath+"gams",snakemake.params.sharedcodepath+"/highres.gms",
+    "gdxCompress=1",
+    "--co2intensity="+str(snakemake.params.co2intensity),
+    "--weather_yr="+str(snakemake.wildcards.year),
+    #"--vre_restrict="+str(snakemake.wildcards.quantile),
+    #"--fx_trans="+snakemake.wildcards.fix_trans,
+    #"--outname="+snakemake.params.outname
+    ]
+    
+process=subprocess.Popen(args, 
+                         stdout=subprocess.PIPE, 
+                         stderr=subprocess.STDOUT,
+                         cwd=snakemake.params.modelpath) 
+
+# Below writes gams output to terminal in realtime and log
+
+with open(snakemake.log[0], "w") as f:
+    while True:
+        line = process.stdout.readline()
+     
+        if not line and process.poll() is not None:
+            break
+        print(line.decode(), end='')
+        f.write(line.decode().rstrip("\n"))
