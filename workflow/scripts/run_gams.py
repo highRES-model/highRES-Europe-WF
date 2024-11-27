f3258ddda0fc8417f6f31433d3d713a1b4c98b28
absolute-afloat <96127884+absolute-afloat@users.noreply.github.com>
2024-11-27 19:11:21 +0100

    Change how GAMS absolute path handling works to remove the issue where one has to keep track of results path depth and feed this in via the config (#68)

--- workflow/scripts/run_gams.py
+++ workflow/scripts/run_gams.py
@@ -4,13 +4,12 @@ import subprocess
 
 args = [
     snakemake.params.gamspath + "gams",
-    snakemake.params.sharedcodepath + "/highres.gms",
+    snakemake.params.sharedcodepath / "highres.gms",
     "gdxCompress=1",
+    "--codefolderpath="+str(snakemake.params.sharedcodepath),
     "--co2intensity=" + str(snakemake.params.co2intensity),
     "--weather_yr=" + str(snakemake.wildcards.year),
-    # "--vre_restrict="+str(snakemake.wildcards.quantile),
-    # "--fx_trans="+snakemake.wildcards.fix_trans,
-    # "--outname="+snakemake.params.outname
+    "--outname="+snakemake.params.outname
 ]
 
 process = subprocess.Popen(
