0875c3a5eb0e87986cf1c08e4b19fb38cc066521
James-Price <james.price@ucl.ac.uk>
2026-04-13 10:58:52 +0100

    [Feature] Add code to aggregate countries into multi-country regions, e.g. BNL =  BE, NL and LU. Driven by "aggregated_countries" param in config.

--- workflow/scripts/build_zones.py
+++ workflow/scripts/build_zones.py
@@ -3,10 +3,16 @@ import numpy as np
 
 zones=np.array(snakemake.params.aggregated_regions)
 
-if snakemake.params.focus_countries:
+if snakemake.params.focus_countries is not None:
  
-    for (key,z) in snakemake.params.focus_countries.items():
+    for key,z in snakemake.params.focus_countries.items():
     
         zones=np.append(zones[zones!=key],z)
+        
+if snakemake.params.aggregated_countries is not None:
+    
+    for key,z in snakemake.params.aggregated_countries.items():
+        
+        zones=np.append(zones[np.isin(zones,z,invert=True)],key)
         
 np.savetxt(snakemake.output[0],zones,fmt="%s")
