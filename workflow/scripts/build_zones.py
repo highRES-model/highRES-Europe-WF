32076646a789e8d869ddee63299e8ea59f7a5d16
James-Price <james.price@ucl.ac.uk>
2026-08-06 14:58:59 +0100

    [Tweak] Add code needed to support config based assignment of zones where UC constraints are turned on.
    Uses config parameter "unit_commitment_zones" which may be a list of zones or string "ALL" if all zones should have UC constraints.

--- workflow/scripts/build_zones.py
+++ workflow/scripts/build_zones.py
@@ -1,4 +1,5 @@
 import numpy as np
+from data2dd_funcs import wrapdd
 
 
 zones=np.array(snakemake.params.aggregated_regions)
@@ -14,5 +15,26 @@ if snakemake.params.aggregated_countries is not None:
     for key,z in snakemake.params.aggregated_countries.items():
         
         zones=np.append(zones[np.isin(zones,z,invert=True)],key)
-        
-np.savetxt(snakemake.output[0],zones,fmt="%s")
+           
+
+zones_out=wrapdd(zones.reshape(-1,1),"z","set")
+
+if snakemake.params.unit_commitment == "ON":
+    uc_zones=snakemake.params.unit_commitment_zones
+    if type(uc_zones)==list:
+        uc_zones=wrapdd(
+            np.array(uc_zones).reshape(-1,1)
+            ,"uc_z",
+            "set"
+            )
+    if type(uc_zones)==str and uc_zones=="ALL":
+        uc_zones=wrapdd(zones.copy().reshape(-1,1)
+                        ,"uc_z","set") 
+   
+    np.savetxt(snakemake.output[0],
+           np.concatenate((zones_out,uc_zones),axis=0)
+           ,fmt="%s")
+else:
+    np.savetxt(snakemake.output[0],
+               zones_out,
+               fmt="%s")
